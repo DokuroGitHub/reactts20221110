6494f9edc4d4562ac5ea14bd7d6712687531672e
DokuroGitHub <67427497+DokuroGitHub@users.noreply.github.com>
2022-11-10 00:33:05 +0700

    1 file modified, 4 deleted

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
+import reportWebVitals from './src/components/reportWebVitals';
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -12,3 +12,5 @@ root.render(
     <App />
   </StrictMode>
 );
+
+reportWebVitals();
